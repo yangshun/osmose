703753b69bdf0f2c028262f8ab57286849612ea6
soedar <soedar.sg@gmail.com>
2013-08-25 15:46:31 +0800

    Added the functions for course

--- api/controllers/CoursesController.js
+++ api/controllers/CoursesController.js
@@ -7,30 +7,52 @@
 
 module.exports = {
   index: function(req, res) {
-
+    return res.api.failure();
   },
   
   show: function(req, res) {
-    var cid = req.param('id');
-    Courses.findOne({id: cid, deleted: false}).done(function(err, course) {
-      if (err || course === undefined) return res.send(404);
-  	  Questions.getQuestionsOfCourse(req.param('id'), function(err, questions) {
-  		  if (err || questions === undefined) return res.send(404);
-  		  course.questions = questions;
-        return res.send(course);
-  	  });
+    var id = req.param('id');
+    Courses.findOne({id: id, deleted: false}).done(function(err, course) {
+      if (err || course == undefined) {
+        return res.api.failure();
+      }
+
+      console.log(course);
+      Questions.getQuestionsOfCourse(id, function(err, questions) {
+        if (err || questions == undefined) {
+          console.log('1' + err)
+          return res.api.failure();
+        }
+        course.questions = questions;
+        return res.api.success({'course': course});
+      })
     });
   },
 
   create: function(req, res) {
-
+    Courses.create(req.body).done(function(err, course) {
+      if (err) {
+        return res.api.failure();
+      }
+      return res.api.success({'course': course});
+    });
   },
 
   update: function(req, res) {
-
+    Courses.update({id: req.param('id')}, req.body).done(function(err, courses) {
+      if (err || courses == undefined) {
+        return res.api.failure();
+      }
+      return res.api.success({'course': courses[0]});
+    });
   },
 
   remove: function(req, res) {
-
+    Courses.destroy({id: req.param('id')}).done(function(err) {
+      if (err) {
+        return res.api.failure();
+      }
+      return res.api.success({});
+    });
   }
 };
