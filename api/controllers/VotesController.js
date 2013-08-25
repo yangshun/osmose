e9654c700c354a275bd3473ae6b014f6090dd8c5
Tony Le <lvtien.dev@gmail.com>
2013-08-25 15:36:42 +0800

    Votes controller

--- api/controllers/VotesController.js
+++ api/controllers/VotesController.js
@@ -7,21 +7,29 @@
 
 module.exports = {  
   index: function(req, res) {
-
+    res.api.failure();
   },
 
   show: function(req, res) {
-
+    res.api.failure();
   },
 
   create: function(req, res) {
-
+    Votes.create(req.body, function(err, vote) {
+      if (err || vote === undefined) res.api.failure();
+      else res.api.success({'vote': vote});
+    });
   },
 
   update: function(req, res) {
-
+    res.api.failure();
   },
   
   remove: function(req, res) {
+    var vid = req.param('id');
+    Votes.destroy(vid, function(err, vote) {
+      if (err) res.api.failure();
+      else res.api.success({'vote': vote});
+    });
   }
 };
