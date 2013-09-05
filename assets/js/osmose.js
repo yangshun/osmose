22bbc25a73e17b7bd57438b97e36b7c2490d1e9b
Chunmun <xilasirc@gmail.com>
2013-09-06 00:42:06 +0800

    Change regex to not catch img tags

--- assets/js/osmose.js
+++ assets/js/osmose.js
@@ -101,7 +101,7 @@ var osmose_markdowns = {
 	    var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
         // www. sans http:// or https://
-        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        var pseudoUrlPattern = /(?:<img.+>)|(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
         // Email addresses
         var emailAddressPattern = /\w+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gim;
